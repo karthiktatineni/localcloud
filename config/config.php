9191336bebbdc81c9d58cab0e7a179fe497e6e0c
Karthik Tatineni <karthiktatineni34@gmail.com>
2026-01-29 00:15:18 +0530

    configurations

---
+++ config/config.php
@@ -0,0 +1,35 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'upgrade.disable-web' => true,
+  'instanceid' => 'oca4evyvkv8e',
+  'passwordsalt' => 'hsKDM55HEIxXwmB5V2ob210JwxGYUo',
+  'secret' => 'm6z+NpXhD5Bc/YPhU5NdLx5JlqJN70MBYymNAw+skdEMKr6h',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost:9090',
+    1 => 'cindy-priority-tape-genuine.trycloudflare.com',
+  ),
+  'overwrite.cli.url' => 'cindy-priority-tape-genuine.trycloudflare.com',
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'sqlite3',
+  'version' => '32.0.5.0',
+  'overwrite.cli.url' => 'http://localhost:9090',
+  'installed' => true,
+);
